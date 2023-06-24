623d832f361748dff03c8b06b733b04f512a90ac
Himanshu Rishi <75842444+Himanshu-Rishi@users.noreply.github.com>
2023-06-24 11:35:30 +0530

    Navbar updated.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,47 +15,18 @@ import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
 import Logout from "@mui/icons-material/Logout";
 import { Menu } from "@mui/material";
-import { useEffect } from "react";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import PersonIcon from "@mui/icons-material/Person";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import { toast } from "react-hot-toast";
 import ReminderContext from "../context/ReminderContext";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
   const handler = React.useContext(ReminderContext);
-  const gapi = window.gapi;
-  const google = window.google;
-
-  const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-  const API_KEY = process.env.REACT_APP_API_KEY;
-  const DISCOVERY_DOC =
-    "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
-  const SCOPES = "https://www.googleapis.com/auth/calendar";
-
-  const accessToken = localStorage.getItem("access_token");
-  const expiresIn = localStorage.getItem("expires_in");
-
-  let gapiInited = false,
-    gisInited = false,
-    tokenClient;
-
-  function gapiLoaded() {
-    gapi.load("client", initializeGapiClient);
-  }
-  function gisLoaded() {
-    tokenClient = google.accounts.oauth2.initTokenClient({
-      client_id: CLIENT_ID,
-      scope: SCOPES,
-      callback: "", // defined later
-    });
-
-    gisInited = true;
-  }
-  useEffect(() => {
-    gapiLoaded();
-    gisLoaded();
-  });
+  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
@@ -101,258 +72,195 @@ const Navbar = () => {
     setAnchorEl(null);
   };
 
-  async function initializeGapiClient() {
-    await gapi.client.init({
-      apiKey: API_KEY,
-      discoveryDocs: [DISCOVERY_DOC],
-    });
-    gapiInited = true;
-
-    if (accessToken && expiresIn) {
-      gapi.client.setToken({
-        access_token: accessToken,
-        expires_in: expiresIn,
-      });
-      // listUpcomingEvents();
-    }
-  }
-
-  //Enables user interaction after all libraries are loaded.
-
-  async function handleAuthClick() {
-    tokenClient.callback = async (resp) => {
-      if (resp.error) {
-        toast.error("Login Failed..!");
-        handler.isLogin = false;
-        throw resp;
-      }
-      // await listUpcomingEvents();
-      const { access_token, expires_in } = gapi.client.getToken();
-      localStorage.setItem("access_token", access_token);
-      localStorage.setItem("expires_in", expires_in);
-    };
-
-    if (!(accessToken && expiresIn)) {
-      // Prompt the user to select a Google Account and ask for consent to share their data
-      // when establishing a new session.
-      await tokenClient.requestAccessToken({ prompt: "consent" });
-      handler.isLogin = true;
-    } else {
-      // Skip display of account chooser and consent dialog for an existing session.
-      tokenClient.requestAccessToken({ prompt: "" });
-    }
-  }
-
-  //Sign out the user upon button click.
-
-  function handleSignoutClick() {
-    const token = gapi.client.getToken();
-    if (token !== null) {
-      google.accounts.oauth2.revoke(token.access_token);
-      gapi.client.setToken("");
-      localStorage.clear();
-    }
-    toast.success("Logout Successfully");
-  }
-
-  // async function listUpcomingEvents() {
-  //   let response;
-  //   try {
-  //     const request = {
-  //       calendarId: "primary",
-  //       timeMin: new Date().toISOString(),
-  //       showDeleted: false,
-  //       singleEvents: true,
-  //       maxResults: 10,
-  //       orderBy: "startTime",
-  //     };
-  //     response = await gapi.client.calendar.events.list(request);
-  //   } catch (err) {
-  //     document.getElementById("content").innerText = err.message;
-  //     return;
-  //   }
-
-  //   const events = response.result.items;
-  //   if (!events || events.length === 0) {
-  //     document.getElementById("content").innerText = "No events found.";
-  //     return;
-  //   }
-  //   // Flatten to string to display
-  //   const output = events.reduce(
-  //     (str, event) =>
-  //       `${str}${event.summary} (${
-  //         event.start.dateTime || event.start.date
-  //       })\n`,
-  //     "Events:\n"
-  //   );
-  //   document.getElementById("content").innerText = output;
-  // }
-
   return (
     <>
-      <div className="navbar_container">
-        <div>
-          <Button
-            ref={anchorRef}
-            id="composition-button"
-            aria-controls={open ? "composition-menu" : undefined}
-            aria-expanded={open ? "true" : undefined}
-            aria-haspopup="true"
-            onClick={handleToggle}
-          >
-            <MenuIcon
-              sx={{ color: "white", marginLeft: "10px", marginTop: "10px" }}
-            ></MenuIcon>
-          </Button>
-          <Popper
-            open={open}
-            anchorEl={anchorRef.current}
-            role={undefined}
-            placement="bottom-start"
-            transition
-            disablePortal
-          >
-            {({ TransitionProps, placement }) => (
-              <Grow
-                {...TransitionProps}
-                style={{
-                  transformOrigin:
-                    placement === "bottom-start" ? "left top" : "left bottom",
-                }}
-              >
-                <Paper sx={{ marginLeft: "15px" }}>
-                  <ClickAwayListener onClickAway={handleClose}>
-                    <MenuList
-                      autoFocusItem={open}
-                      id="composition-menu"
-                      aria-labelledby="composition-button"
-                      onKeyDown={handleListKeyDown}
-                    >
-                      <Link to="/kickstart">
-                        <MenuItem onClick={handleClose}>Kickstart</MenuItem>
-                      </Link>
-                      <Link to="/searchuser">
-                        <MenuItem onClick={handleClose}>Search User</MenuItem>
-                      </Link>
-                      <Link to="/about">
-                        <MenuItem onClick={handleClose}>About</MenuItem>
-                      </Link>
-                    </MenuList>
-                  </ClickAwayListener>
-                </Paper>
-              </Grow>
-            )}
-          </Popper>
-        </div>
-        <div className="account_box">
-          <React.Fragment>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                textAlign: "center",
-              }}
+      {!isLoading && (
+        <div className="navbar_container">
+          <div>
+            <Button
+              ref={anchorRef}
+              id="composition-button"
+              aria-controls={open ? "composition-menu" : undefined}
+              aria-expanded={open ? "true" : undefined}
+              aria-haspopup="true"
+              onClick={handleToggle}
             >
-              <Tooltip title="Account settings">
-                <IconButton
-                  onClick={handleClick}
-                  size="small"
-                  sx={{ ml: 2 }}
-                  aria-controls={open ? "account-menu" : undefined}
-                  aria-haspopup="true"
-                  aria-expanded={open ? "true" : undefined}
+              <MenuIcon
+                sx={{ color: "white", marginLeft: "10px", marginTop: "10px" }}
+              ></MenuIcon>
+            </Button>
+            <Popper
+              open={open}
+              anchorEl={anchorRef.current}
+              role={undefined}
+              placement="bottom-start"
+              transition
+              disablePortal
+            >
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  {...TransitionProps}
+                  style={{
+                    transformOrigin:
+                      placement === "bottom-start" ? "left top" : "left bottom",
+                  }}
                 >
-                  <ListItemIcon>
-                    <AccountCircleIcon
-                      fontSize="large"
-                      sx={{ color: "white" }}
-                    />
-                  </ListItemIcon>
-                </IconButton>
-              </Tooltip>
-            </Box>
-            <div className="home_profile_section">
-              <Menu
-                anchorEl={anchorEl}
-                id="account-menu"
-                open={open_account}
-                onClose={handleClose_account}
-                onClick={handleClose_account}
-                PaperProps={{
-                  elevation: 0,
-                  sx: {
-                    overflow: "visible",
-                    filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-                    mt: 1.5,
-                    "& .MuiAvatar-root": {
-                      width: 32,
-                      height: 32,
-                      ml: -0.5,
-                      mr: 1,
-                    },
-                    "&:before": {
-                      content: '""',
-                      display: "block",
-                      position: "absolute",
-                      top: 0,
-                      right: 14,
-                      width: 10,
-                      height: 10,
-                      bgcolor: "background.paper",
-                      transform: "translateY(-50%) rotate(45deg)",
-                      zIndex: 0,
-                    },
-                  },
+                  <Paper sx={{ marginLeft: "15px" }}>
+                    <ClickAwayListener onClickAway={handleClose}>
+                      <MenuList
+                        autoFocusItem={open}
+                        id="composition-menu"
+                        aria-labelledby="composition-button"
+                        onKeyDown={handleListKeyDown}
+                        size="large"
+                      >
+                        <Link to="/kickstart">
+                          <MenuItem onClick={handleClose}>Kickstart</MenuItem>
+                        </Link>
+                        <Link to="/searchuser">
+                          <MenuItem onClick={handleClose}>Search User</MenuItem>
+                        </Link>
+                        <Link to="/about">
+                          <MenuItem onClick={handleClose}>About</MenuItem>
+                        </Link>
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
+          </div>
+          <div className="account_box">
+            <React.Fragment>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  textAlign: "center",
                 }}
-                transformOrigin={{ horizontal: "right", vertical: "top" }}
-                anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
               >
-                <button
-                  className="button_to_text"
-                  disabled={accessToken && expiresIn}
-                  onClick={handleAuthClick}
+                <Tooltip title="Account settings">
+                  <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    sx={{ ml: 2 }}
+                    aria-controls={open ? "account-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                  >
+                    <ListItemIcon>
+                      <AccountCircleIcon
+                        fontSize="large"
+                        sx={{ color: "white" }}
+                      />
+                    </ListItemIcon>
+                  </IconButton>
+                </Tooltip>
+              </Box>
+              <div className="home_profile_section">
+                <Menu
+                  anchorEl={anchorEl}
+                  id="account-menu"
+                  open={open_account}
+                  onClose={handleClose_account}
+                  onClick={handleClose_account}
+                  PaperProps={{
+                    elevation: 0,
+                    sx: {
+                      overflow: "visible",
+                      filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                      mt: 1.5,
+                      "& .MuiAvatar-root": {
+                        width: 32,
+                        height: 32,
+                        ml: -3.5,
+                        mr: 2,
+                      },
+                      "&:before": {
+                        content: '""',
+                        display: "block",
+                        position: "absolute",
+                        top: 0,
+                        right: 35,
+                        width: 10,
+                        height: 10,
+                        bgcolor: "background.paper",
+                        transform: "translateY(-50%) rotate(45deg)",
+                        zIndex: 0,
+                      },
+                    },
+                  }}
+                  transformOrigin={{ horizontal: "right", vertical: "top" }}
+                  anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
                 >
-                  <MenuItem onClick={handleClose_account}>
-                    <ListItemIcon>
-                      <AccountCircleIcon fontSize="medium" />
-                    </ListItemIcon>
-                    Login
-                  </MenuItem>
-                </button>
-                <Divider />
-                <a href="https://rishiportfolio.vercel.app">
-                  <MenuItem onClick={handleClose_account}>
-                    <ListItemIcon>
-                      <PersonIcon fontSize="medium" />
-                    </ListItemIcon>
-                    About Developer
-                  </MenuItem>
-                </a>
-                <a href="https://github.com/Himanshu-Rishi">
-                  <MenuItem onClick={handleClose_account}>
-                    <ListItemIcon>
-                      <GitHubIcon fontSize="medium" />
-                    </ListItemIcon>
-                    Github
-                  </MenuItem>
-                </a>
-                <button
-                  className="button_to_text"
-                  disabled={!accessToken && !expiresIn}
-                  onClick={handleSignoutClick}
-                >
-                  <MenuItem onClick={handleClose_account}>
-                    <ListItemIcon>
-                      <Logout fontSize="medium" />
-                    </ListItemIcon>
-                    Logout
-                  </MenuItem>
-                </button>
-              </Menu>
-            </div>
-          </React.Fragment>
+                  {!isAuthenticated && (
+                    <button
+                      className="button_to_text"
+                      onClick={() => loginWithRedirect()}
+                    >
+                      <MenuItem onClick={handleClose_account}>
+                        <ListItemIcon>
+                          <AccountCircleIcon fontSize="medium" />
+                        </ListItemIcon>
+                        Login
+                      </MenuItem>
+                    </button>
+                  )}
+                  {isAuthenticated && (
+                    <MenuItem onClick={handleClose_account}>
+                      <ListItemIcon>
+                        {user.picture ? (
+                          <img
+                            className="user__image"
+                            src={user.picture}
+                            alt={user.name}
+                          />
+                        ) : (
+                          <AccountCircleIcon fontSize="medium" />
+                        )}
+                      </ListItemIcon>
+                      <p>{user.name}</p>
+                    </MenuItem>
+                  )}
+                  <a href="https://rishiportfolio.vercel.app">
+                    <MenuItem onClick={handleClose_account}>
+                      <ListItemIcon>
+                        <PersonIcon fontSize="medium" />
+                      </ListItemIcon>
+                      About Developer
+                    </MenuItem>
+                  </a>
+                  <a href="https://github.com/Himanshu-Rishi">
+                    <MenuItem onClick={handleClose_account}>
+                      <ListItemIcon>
+                        <GitHubIcon fontSize="medium" />
+                      </ListItemIcon>
+                      Github
+                    </MenuItem>
+                  </a>
+                  {isAuthenticated && (
+                    <button
+                      className="button_to_text"
+                      onClick={() =>
+                        logout({
+                          logoutParams: { returnTo: window.location.origin },
+                        })
+                      }
+                    >
+                      <MenuItem onClick={handleClose_account}>
+                        <ListItemIcon>
+                          <Logout fontSize="medium" />
+                        </ListItemIcon>
+                        Logout
+                      </MenuItem>
+                    </button>
+                  )}
+                </Menu>
+              </div>
+            </React.Fragment>
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Auth0Provider
+    domain="dev-pf3lr4v3cl1y4l6u.us.auth0.com"
+    clientId="pi9sGjMXqMNedPpwKayrhFg0OrEdtbYO"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
+  </Auth0Provider>
 );
